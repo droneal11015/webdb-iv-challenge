4a62957508b66517898eb2829d84fd01429627cf
droneal11015 <droneal11015@gmail.com>
2019-06-13 17:31:41 -0400

    completed migrations

--- migrations/20190613170036_instructions.js
+++ migrations/20190613170036_instructions.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('instructions', table => {
+        table.increments();
+        table.string('instructions').notNullable();
+        table.integer('order').unsigned();
+        table.integer('recipe_id').unsigned();
+        table.foreign('recipe_id').references('id').on('recipes');
+    })
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('instructions');
+  };
